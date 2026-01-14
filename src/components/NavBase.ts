edf26a0405be6ed7127220185604864d6590c93e
Jindřich Ochmann <jindrich.ochmann@gmail.com>
2026-01-14 12:21:34 +0100

    notSure2

---
+++ src/components/NavBase.ts
@@ -0,0 +1,15 @@
+export default [
+  {
+    title: "Projects",
+    url: "/projects",
+  },
+  {
+    title: "Tutorials",
+    url: "/tutorials",
+    sublinks: [
+      { title: "Git", url: "/tutorials/git" },
+      { title: "Markdown", url: "/tutorials/markdown" },
+      { title: "C/C++", url: "/tutorials/c/c" },
+    ],
+  },
+];
--- src/components/nav-base.ts
+++
@@ -1,5 +0,0 @@
-export default [
-  { title: "Projekty", href: "/projects" },
-  { title: "Kontakt", href: "/contacts" },
-  { title: "Dev", href: "/dev" },
-];
