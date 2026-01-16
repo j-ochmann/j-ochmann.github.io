1a0a46b3363a9c952897c93ffd27bedc6e48499a
Jindřich Ochmann <jindrich.ochmann@gmail.com>
2026-01-16 16:21:55 +0100

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import rehypeMermaid from 'rehype-mermaid';
 import d2 from 'astro-d2'; // Předpokládá se instalace integrace
+import react from '@astrojs/react';
+import { fileURLToPath } from 'node:url'; // Importujte pro práci s cestami
 
 export default defineConfig({
   site: 'https://j-ochmann.github.io',
@@ -25,64 +28,72 @@ export default defineConfig({
     ]
   },
   integrations: [
+    react(),
     d2(),
     starlight({
-      title: 'DevBook',
-      social: [
-        { icon: 'github', label: 'GitHub', href: 'https://github.com/j-ochmann' }
-      ],
-      sidebar: [
-        {
-          label: 'Quick Reference',
-          autogenerate: { directory: 'quick_reference' },
-          collapsed: true,
-        },
-        // {
-        //   label: 'Design Patterns',
-        //   autogenerate: { directory: 'design_patterns' },
-        // },
-        {
-          label: 'Design Patterns',
-          collapsed: true,
-          //link: '/design_patterns/',
-          items: [
-            { 
-              label: 'Overview',
-              link: '/design_patterns/',
-            },
-            {
-              label: 'Creational',
-              autogenerate: { directory: 'design_patterns/creational/' },
-              collapsed: true, 
-            },
-            { label: 'Structural',
-              autogenerate: { directory: 'design_patterns/structural/' },
-              collapsed: true,
-            },
-            { label: 'Behavioral',
-              autogenerate: { directory: 'design_patterns/behavioral' },
-              collapsed: true,
-            },
-          ],
-        },
-        {
-          label: 'External Resources',
-          link: '/external_resources/',
-        },
-        {
-          label: '3D Graph Example',
-          link: '/3d-graph-example/',
-        },
-        {
-          label: 'Artificial Intelligence',
-          link: '/ai/',
-        },
-        {
-          label: 'Courses',
-          link: '/courses/',
-          badge: 'Free',
-        },
-      ],
-    }),
-  ],
+    title: 'DevBook',
+    social: [
+      { icon: 'github', label: 'GitHub', href: 'https://github.com/j-ochmann' }
+    ],
+    sidebar: [
+      {
+        label: 'Quick Reference',
+        autogenerate: { directory: 'quick_reference' },
+        collapsed: true,
+      },
+      // {
+      //   label: 'Design Patterns',
+      //   autogenerate: { directory: 'design_patterns' },
+      // },
+      {
+        label: 'Design Patterns',
+        collapsed: true,
+        //link: '/design_patterns/',
+        items: [
+          { 
+            label: 'Overview',
+            link: '/design_patterns/',
+          },
+          {
+            label: 'Creational',
+            autogenerate: { directory: 'design_patterns/creational/' },
+            collapsed: true, 
+          },
+          { label: 'Structural',
+            autogenerate: { directory: 'design_patterns/structural/' },
+            collapsed: true,
+          },
+          { label: 'Behavioral',
+            autogenerate: { directory: 'design_patterns/behavioral' },
+            collapsed: true,
+          },
+        ],
+      },
+      {
+        label: 'External Resources',
+        link: '/external_resources/',
+      },
+      {
+        label: '3D Graph Example',
+        link: '/3d-graph-example/',
+      },
+      {
+        label: 'Artificial Intelligence',
+        link: '/ai/',
+      },
+      {
+        label: 'Courses',
+        link: '/courses/',
+        badge: 'Free',
+      },
+    ],
+  })],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
